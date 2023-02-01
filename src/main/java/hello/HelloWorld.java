8ed433d579862e77f9e85b62a20a6a482f8a0abb
gitmor <16514449+gitmor@users.noreply.github.com>
2023-02-01 03:08:12 -0600

    add if

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -10,7 +10,10 @@ public class HelloWorld {
   public static void main(String[] args) {
     Greeter greeter = new Greeter();
     //System.out.println(greeter.sayHello());
-	logger.debug(greeter.sayHello());
+	if (logger.isDebugEnabled()) {
+		logger.debug(greeter.sayHello());
+	}
+	
     t2();
   }
 
