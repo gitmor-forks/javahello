2b85279d2e1c7f6830fadd34bf3e1c0f3c1fabf6
gitmor <16514449+gitmor@users.noreply.github.com>
2023-02-01 02:31:44 -0600

    log4j

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,9 +1,19 @@
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.*;
+
 package hello;
 
 public class HelloWorld {
+ 
+   static Logger log = Logger.getLogger(HelloWorld.class.getName());
+   	
   public static void main(String[] args) {
     Greeter greeter = new Greeter();
-    System.out.println(greeter.sayHello());
+    //System.out.println(greeter.sayHello());
+	log.debug(greeter.sayHello());
     t2();
   }
 
